07ae8f3da1dcec0a75b1f49c4d94194b26ce4713
George Grainger <GraingerGY@cardiff.ac.uk>
2020-02-09 16:49:52 +0000

    Implement tileDrawing

--- sudoku.py
+++ sudoku.py
@@ -2,11 +2,13 @@ import pygame
 
 class tile(object):
     "Class to represent a sudoku tile."
-    def __init__(self, value, position, tileSize, MinMax = (0, 10)):
+    def __init__(self, value, position, tileSize, tileColor, window, MinMax = (0, 10)):
         self.value = value
         self.__position = position
         self.__size = tileSize
+        self.__color = tileColor
         self.__MinMax = MinMax
+        self.__window = window
 
     def click(self, button):
         """Handle the click action on this tile.\n
@@ -15,7 +17,7 @@ class tile(object):
 
     def draw(self):
         "Draw the tile on screen."
-        pass
+        pygame.draw.rect(self.__window, self.__color, (self.__position[0] + 1, self.__position[1] + 1, self.__size - 2, self.__size - 2))
 
     def change(self, change_to):
         "Changes the value this tile holds if it is within the acceptable range."
@@ -24,25 +26,54 @@ class tile(object):
 
 class tileGroup(object):
     "Class to represent the groups of 9 tiles in sudoku."
-    def __init__(self, position, tileSize):
+    def __init__(self, position, tileSize, tileColor, window):
         self.position = position
-        self.tiles = [tile(None, (x + 1,y + 1), tileSize) for x in range(position[0], 3 * tileSize, tileSize) for y in range(position[1], 3 * tileSize, tileSize)]
+        #Create the tiles for this tileGroup as specified by position
+        self.tiles = [tile(None, (x + 1,y + 1), tileSize - 2, tileColor, window) for x in range(position[0], position[0] +  3 * tileSize, tileSize) for y in range(position[1], position[1] + 3 * tileSize, tileSize)]
         self.__tileSize = tileSize
 
     def draw(self):
         "Draw all tiles from this tilegroup on the display."
-        pass
+        for tile in self.tiles:
+            tile.draw()
 
 class game(object):
-    def __init__(self, tileSize):
-        "Size attribute sets the maximum size of the game window."
+    """Object represents the entire game.\n
+    tileSize arguments changes the size of the tiles. (size in px).\n
+    All color arguments should be passed tuple in form (R,G,B).\n
+    tileColor sets the color of the tiles.\n
+    bgColor sets the background color of the window.\n
+    boundaryColor sets the color of the boundary between the tileGroups."""
+    def __init__(self, tileSize, tileColor, bgColor, boundaryColor):
         pygame.init()
         pygame.font.init()
         self.__tileSize = tileSize
         self.__windowSize = (tileSize * 9, tileSize * 9)
         self.__window = pygame.display.set_mode(self.__windowSize)
+        #Create 9 tile groups
+        self.__tileGroups = [tileGroup((x, y), tileSize, tileColor, self.__window) for x in range(0, tileSize * 9, tileSize * 3) for y in range(0, tileSize * 9, tileSize * 3)]
+        #Set color attributes.
+        self.__bgColor = bgColor
+        self.boundaryColor = boundaryColor
         pygame.display.set_caption("Sudoku")
     
+    def __draw(self):
+        """Draw everything on the window."""
+        #Define draw subfunctions.
+        def draw_tileGroups():
+            """Call the draw methods of all of the tileGroups."""
+            for tileGroup in self.__tileGroups:
+                tileGroup.draw()
+        def draw_boundaries():
+            """Draw the boundaries between the tileGroups."""
+            pass
+        
+        #Call functions
+        self.__window.fill(self.__bgColor)
+        draw_tileGroups()
+        draw_boundaries()
+        pygame.display.update()
+
     def open(self):
         "Open main event loop."
         self.__running = True
@@ -59,7 +90,7 @@ class game(object):
                     keyDown = True
                 elif event.type == pygame.KEYUP:
                     keyDown = False
-            pygame.display.update()
+            self.__draw()
         pygame.quit()
         quit()
 
@@ -68,5 +99,5 @@ class game(object):
         self.__running = False
 
 if __name__ == "__main__":
-    game = game(50)
+    game = game(50, (255,255,255), (128,128,128), (0,0,0))
     game.open()
